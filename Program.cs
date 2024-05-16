635524d251b6648b0653d6aaca1a3a6118d6b3ef
Symon Kipkemei <symonkipkemei@gmail.com>
2024-05-16 20:23:35 +0300

    External application added

--- Program.cs
+++ Program.cs
@@ -7,14 +7,55 @@ using System.IO;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.Attributes;
-
+using System.Reflection;
+using System.Windows.Media.Imaging;
 
 namespace SnapMe
 {
+    
+
+
+    public class App : IExternalApplication
+    {
+
+        private void AddRibbonButton(UIControlledApplication application)
+        {
+            // Ribbon Panel
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel("SnapMe");
+            string assembly = Assembly.GetExecutingAssembly().Location;
+
+
+            TaskDialog.Show("REVIT", assembly);
+            //Button data
+            PushButtonData data = new PushButtonData("SnapMe", "SnapMe", assembly, "SnapMe.Program");
+            
+
+            PushButton button = ribbonPanel.AddItem(data) as PushButton;
+            button.ToolTip = "Automatically snap images like a pro";
+            Uri uri = new Uri("pack://application:,,,/SnapMe;component/snapme.png");
+            BitmapImage image = new BitmapImage(uri);
+            button.LargeImage = image;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            AddRibbonButton(application);
+
+            return Result.Succeeded;
+        }
+
+
+
+    }
+
     // Attributes
     [Transaction(TransactionMode.Manual)]
     [Regeneration(RegenerationOption.Manual)]
-
 
     public class Program : IExternalCommand
     {
